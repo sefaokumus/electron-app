d48392c63000cbcc2319880ea2a6bceb7532bacd
Sefa Okumuş <sefa@okumus.com>
2022-07-08 22:45:50 +0300

    aaa.

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ async function puppeteerInit() {
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  await page.goto('https://google.com.com');
+  await page.goto('https://google.com');
   await page.screenshot({path: 'example.png'});
 
   // await browser.close();
